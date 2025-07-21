9c5994594e60364dcef69189249aee5fc20e1037
yugure <109891005+yugure-orca@users.noreply.github.com>
2025-07-21 16:17:03 +0900

    init

---
+++ programs/instructions-reflection/src/lib.rs
@@ -0,0 +1,89 @@
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::{instruction::Instruction, program::invoke};
+
+#[cfg(not(feature = "child"))]
+declare_id!("H4vTCnnisqDnZW2ByCcUVbxuB73Xni4D4VzAt7ywuoHj");
+
+#[cfg(feature = "child")]
+declare_id!("ExQUucxSnQbAuLoTQxKo4m5woiMmh1YiLgZ2UW11zhEN");
+
+#[program]
+pub mod instructions_reflection {
+    use anchor_lang::solana_program::instruction::get_processed_sibling_instruction;
+
+    use super::*;
+
+    pub fn parent_call(ctx: Context<ParentCall>) -> Result<()> {
+        invoke(&Instruction {
+            program_id: ctx.accounts.memo_program.key(),
+            accounts: vec![],
+            data: vec![b'm', b'e', b'm', b'o', b'#', b'p', b'.', b'1'], // memo#p.1
+        }, &vec![])?;
+
+        invoke(&Instruction {
+            program_id: ctx.accounts.memo_program.key(),
+            accounts: vec![],
+            data: vec![b'm', b'e', b'm', b'o', b'#', b'p', b'.', b'2'], // memo#p.2
+        }, &vec![])?;
+
+        invoke(&Instruction {
+            program_id: ctx.accounts.child_program.key(),
+            accounts: vec![
+                AccountMeta {
+                    pubkey: ctx.accounts.memo_program.key(),
+                    is_signer: false,
+                    is_writable: false,
+                },
+            ],
+            data: vec![0xf8, 0x51, 0xda, 0x23, 0x3c, 0x1d, 0x3d, 0x90], // disc for child_call
+        }, &vec![])?;
+
+        // get processed sibling
+        msg!("Processing sibling instructions in parent_call...");
+        let mut index = 0;
+        while let Some(instruction) = get_processed_sibling_instruction(index) {
+            msg!("[parent] Sibling instruction program: {:?}, data: {:?}", instruction.program_id, String::from_utf8(instruction.data).unwrap());
+            index += 1;
+        }
+
+        Ok(())
+    }
+
+    pub fn child_call(ctx: Context<ChildCall>) -> Result<()> {
+        invoke(&Instruction {
+            program_id: ctx.accounts.memo_program.key(),
+            accounts: vec![],
+            data: vec![b'm', b'e', b'm', b'o', b'#', b'c', b'.', b'1'], // memo#c.1
+        }, &vec![])?;
+
+        invoke(&Instruction {
+            program_id: ctx.accounts.memo_program.key(),
+            accounts: vec![],
+            data: vec![b'm', b'e', b'm', b'o', b'#', b'c', b'.', b'2'], // memo#c.2
+        }, &vec![])?;
+
+        // get processed sibling
+        msg!("Processing sibling instructions in child_call...");
+        let mut index = 0;
+        while let Some(instruction) = get_processed_sibling_instruction(index) {
+            msg!("[child] Sibling instruction program: {:?}, data: {:?}", instruction.program_id, String::from_utf8(instruction.data).unwrap());
+            index += 1;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct ParentCall<'info> {
+    /// CHECK: just for test
+    pub memo_program: UncheckedAccount<'info>,
+    /// CHECK: just for test
+    pub child_program: UncheckedAccount<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ChildCall<'info> {
+    /// CHECK: just for test
+    pub memo_program: UncheckedAccount<'info>,
+}
---
+++ tests/instructions-reflection.ts
@@ -0,0 +1,24 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { InstructionsReflection } from "../target/types/instructions_reflection";
+
+describe("instructions-reflection", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.InstructionsReflection as Program<InstructionsReflection>;
+
+  const MEMO_PROGRAM_ID = new anchor.web3.PublicKey("MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr");
+
+  it("call parent", async () => {
+    // Add your test here.
+    const tx = await program.methods.parentCall().accounts({
+      memoProgram: MEMO_PROGRAM_ID,
+      childProgram: new anchor.web3.PublicKey("ExQUucxSnQbAuLoTQxKo4m5woiMmh1YiLgZ2UW11zhEN"),
+    }).preInstructions([
+      { programId: MEMO_PROGRAM_ID, keys: [], data: Buffer.from("memo#t.1") },
+      { programId: MEMO_PROGRAM_ID, keys: [], data: Buffer.from("memo#t.2") },
+    ]).rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
